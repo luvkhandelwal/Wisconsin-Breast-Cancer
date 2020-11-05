d56a0de9acf02b506fd9f503b19e3bc989d971cf
Luv Khandelwal <khandelwalluv1997@gmail.com>
2020-11-06 00:21:17 +0530

    almost ready

--- code.R
+++ code.R
@@ -1,20 +1,52 @@
 dev.off()
+rm(list = ls())
 data=read.csv("C://Users//luv//Desktop//Online Courses//Kaggle//Breast Cancer Wisconsin//data.csv")
 
 #Exploratory
+
+for (i in ncol(data))
+{
+if (any(is.na(data[,i])))
+  data=data[,-i]
+}
+
+#Data Partition
 num_data=data[,3:33]
 num_data$result=as.numeric(data$diagnosis=='M')
 
-corr=cor(num_data)
+data1=sample.split(data,SplitRatio = 0.8)
+
+train=subset(data,data1==TRUE)
+test=subset(data,data1==FALSE)
+
+train$diagnosis=as.numeric(train$diagnosis=='M')
+test$diagnosis=as.numeric(test$diagnosis=='M')
+
+#Correlation Analysis
+corr=cor(test[,-1])
 corrplot(corr,tl.cex = 0.5)
 
-num_data<-num_data[,-31]
+#Logistic Regression
 
-g<-glm(result~.,family = binomial(link = 'logit'),data = num_data)
+
+g<-glm(diagnosis~.,family = binomial(link = 'logit'),data = train)
 summary(g)
-pred_data<-data.frame(pmalignant=g$fitted.values,malignant=num_data$result)
-pred_data<-pred_data[order(pred_data$pmalignant,decreasing = FALSE),]
-pred_data$rank<-1:nrow(pred_data)
+ 
+#Fitness 
+
+gof<-hoslem.test(g$y,fitted(g))  
+gof
+?hoslem.test
+
+
+#Testing
+t<-predict.glm(object=g,newdata=test,type = "response")
+
+t=ifelse(test=t>0.5,yes = 1, no=0 )
+table(test$diagnosis,t)
+cat("The accuracy is",116/126*100)
+#Plotting Logistic Data
+
 
 
 ggplot(data = pred_data,aes(x=rank,y=pmalignant))+
